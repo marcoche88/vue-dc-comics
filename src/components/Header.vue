80445d76141e38e2d69220e1926e7bde9ea5585f
marcoche88 <marco.che@hotmail.it>
2021-09-06 17:19:57 +0200

    menu vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,16 +5,11 @@
     </figure>
     <nav class="h-100">
       <ul>
-        <li><a href="#">Characters</a></li>
-        <li><a class="active" href="#">Comics</a></li>
-        <li><a href="#">Movies</a></li>
-        <li><a href="#">Tv</a></li>
-        <li><a href="#">Games</a></li>
-        <li><a href="#">Collectibles</a></li>
-        <li><a href="#">Videos</a></li>
-        <li><a href="#">Fans</a></li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Shop</a></li>
+        <li v-for="(link, index) in links" :key="index">
+          <a :href="link.url" :class="{ active: link.current }">{{
+            link.text
+          }}</a>
+        </li>
       </ul>
     </nav>
   </header>
@@ -23,6 +18,62 @@
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      links: [
+        {
+          text: "Characters",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Comics",
+          url: "#",
+          current: true,
+        },
+        {
+          text: "Movies",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Games",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Collectibles",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Videos",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Fans",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "News",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Shop",
+          url: "#",
+          current: false,
+        },
+      ],
+    };
+  },
 };
 </script>
 
