2a984801bb2ccf329da34694698d070f2cb4d860
marcoche88 <marco.che@hotmail.it>
2021-09-06 15:50:04 +0200

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.h-100 {
+  height: 100%;
+}
+
+body {
+  font-family: sans-serif;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+li {
+  list-style-type: none;
+}
+
+li a {
+  text-decoration: none;
+  color: #666;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+  <header class="container">
+    <figure>
+      <img src="@/assets/img/dc-logo.png" alt="logo" />
+    </figure>
+    <nav class="h-100">
+      <ul>
+        <li><a href="#">Characters</a></li>
+        <li><a class="active" href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">Tv</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Collectibles</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">Fans</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Shop</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+header {
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+figure {
+  padding: 20px 0;
+  width: 60px;
+}
+
+ul {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+ul li {
+  display: inline-block;
+  height: 100%;
+}
+
+header a {
+  display: inline-block;
+  padding: 0 10px;
+  line-height: 97px;
+  font-size: 10px;
+  font-weight: bold;
+  text-transform: uppercase;
+  border-bottom: 3px solid transparent;
+}
+
+header a:hover,
+header a.active {
+  color: rgb(48, 132, 241);
+  border-bottom: 3px solid rgb(48, 132, 241);
+}
+</style>
